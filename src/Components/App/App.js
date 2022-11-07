5054d1dac5759fa2fe82c02690a948ffaedfe452
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2022-11-07 19:18:07 +0100

    first

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import "../BusinessList/BusinessList";
+import "../SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
+import BusinessList from "../BusinessList/BusinessList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ravenous Restaurant Finder</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
